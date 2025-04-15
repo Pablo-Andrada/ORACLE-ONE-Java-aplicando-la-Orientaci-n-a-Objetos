307e139a225d19c6cfbb128778725c48c0a852e2
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-04-15 01:10:08 -0300

    Modelando ScreenMatch

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,4 +3,21 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    int cantidadDeEvaluaciones;
+
+
+
+    double sumaDeLasEvaluaciones;
+    void muestraFichaTecnica(){
+        System.out.println("El nombre de la Pelicula es: "+ nombre);
+        System.out.println("Su fecha de lanzamiento fue: " + fechaDeLanzamiento);
+        System.out.println("Duracion en minutos: " + duracionEnMinutos);
+    }
+    void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        cantidadDeEvaluaciones ++;
+    }
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones / cantidadDeEvaluaciones;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -5,7 +5,20 @@ public class Principal {
         miPelicula.fechaDeLanzamiento=2021;
         miPelicula.duracionEnMinutos=220;
 
-        System.out.println("Mi Pelicula es: "+ miPelicula.nombre);
-        System.out.println("Su fecha de lanzamiento fue: " + miPelicula.fechaDeLanzamiento);
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(9);
+
+        System.out.println("Suma de las evaluaciones: "+ miPelicula.sumaDeLasEvaluaciones);
+        System.out.println("Total de las evaluaciones: "+ miPelicula.cantidadDeEvaluaciones);
+        System.out.println("Puntaje promedio: "+ miPelicula.calculaMedia());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 2001;
+        otraPelicula.duracionEnMinutos = 190;
+
+        otraPelicula.muestraFichaTecnica();
     }
 }
