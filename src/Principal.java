86423fcd0378c4034cec003f61850b2d39743169
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-04-15 02:12:40 -0300

    ControlandoAccesoALaEscrituraDeDatos

--- src/Principal.java
+++ src/Principal.java
@@ -1,24 +1,20 @@
+import com.ORACLE.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre="InterStellar";
-        miPelicula.fechaDeLanzamiento=2021;
-        miPelicula.duracionEnMinutos=220;
+        miPelicula.setNombre("InterStellar");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(220);
+        miPelicula.setIncluidoEnElPlan(true);
 
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(10);
         miPelicula.evalua(10);
         miPelicula.evalua(9);
 
-        System.out.println("Suma de las evaluaciones: "+ miPelicula.sumaDeLasEvaluaciones);
-        System.out.println("Total de las evaluaciones: "+ miPelicula.cantidadDeEvaluaciones);
+        System.out.println("Total de las evaluaciones: "+ miPelicula.getCantidadDeEvaluaciones());
         System.out.println("Puntaje promedio: "+ miPelicula.calculaMedia());
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.fechaDeLanzamiento = 2001;
-        otraPelicula.duracionEnMinutos = 190;
-
-        otraPelicula.muestraFichaTecnica();
     }
 }
